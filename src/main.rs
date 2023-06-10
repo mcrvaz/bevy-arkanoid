fb5ea4b644d3d368d8749e93316610bfa13cbf8b
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2023-06-10 02:03:15 -0300

    feat: plugins adjustments

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,19 @@ use bevy_rapier2d::prelude::*;
 use crate::game_systems::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
+    let plugins = DefaultPlugins
+        .set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Arkanoid!".to_string(),
                 resolution: (1280.0, 720.0).into(),
                 ..default()
             }),
             ..default()
-        }))
-        // .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        })
+        .set(ImagePlugin::default_nearest());
+
+    App::new()
+        .add_plugins(plugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .insert_resource(ClearColor(Color::BLACK))
         // .add_plugin(RapierDebugRenderPlugin::default())
